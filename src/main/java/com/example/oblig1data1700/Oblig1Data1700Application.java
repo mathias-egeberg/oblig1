cbefc7027828ed98b4fddffdfe5b0481d96b308f
Mathais <mathias.egeberg@hotmail.no>
2024-02-12 10:39:01 +0100

    Fist commit

---
+++ src/main/java/com/example/oblig1data1700/Oblig1Data1700Application.java
@@ -0,0 +1,13 @@
+package com.example.oblig1data1700;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig1Data1700Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig1Data1700Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oblig1data1700/Oblig1Data1700ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig1data1700;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1Data1700ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
